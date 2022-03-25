4fb6c6dc0504bbef48b915969cca2b3121562e74
Eduardo Araújo <99681876+eduardoaraujo006@users.noreply.github.com>
2022-03-25 00:11:26 -0300

    Update Semáforo.ino

--- Semáforo.ino
+++ Semáforo.ino
@@ -8,12 +8,12 @@ void setup()
 void loop()
 {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(5000); // Wait for 1000 millisecond(s)
+  delay(5000);
   digitalWrite(LED_BUILTIN, LOW);
   digitalWrite(12, HIGH);
-  delay(5000); // Wait for 1000 millisecond(s)
+  delay(5000);
   digitalWrite(12, LOW);
   digitalWrite(11, HIGH);
-  delay(5000); // Wait for 1000 millisecond(s)
+  delay(5000);
   digitalWrite(11, LOW);
 }
